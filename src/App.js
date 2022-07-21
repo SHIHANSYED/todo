99d84a3a05346601e149ddb70f870f3af2fb93f2
SHIHAN SYED <shihansyedali@gmail.com>
2022-07-21 14:53:54 +0530

    add some codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [input , setNewInput] =useState('');
+const[addList ,setNewList ] = useState([]);
+
+const handleChange=(event)=>{ setNewInput(event.target.value)}
+const addTask=()=>{    const newAddList=[...addList , input]     
+setNewList(newAddList)     }
+
   return (
+
+
+
     <div className="App">
+      <div className='addTask'>
+<input onChange={handleChange}/>
+<button onClick={addTask}>Add TAsk</button>
+    </div>
+
+
+
+      <div className='list' key={addList.task}> 
+      {addList.map((task)=>{ return <h2>{task}</h2>} )}
       
+      
+      </div>
+
+    
     </div>
   );
 }
