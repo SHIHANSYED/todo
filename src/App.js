d767b297fad271244db9e48120ab4b75b4f9dfe4
SHIHAN SYED <shihansyedali@gmail.com>
2022-07-22 02:22:45 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-
-import { useState } from 'react';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Task } from "./Task";
 
 function App() {
-const [input , setNewInput] =useState('');
-const[addList ,setNewList ] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-const handleChange=(event)=>{ setNewInput(event.target.value)}
-const addTask=()=>{    const newAddList=[...addList , input]     
-setNewList(newAddList)     }
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
 
   return (
-
-
-
     <div className="App">
-      <div className='addTask'>
-<input onChange={handleChange}/>
-<button onClick={addTask}>Add TAsk</button>
-    </div>
-
-
-
-      <div className='list' key={addList.task}> 
-      {addList.map((task)=>{ return <h2>{task}</h2>} )}
-      
-      
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button className=" bg-red-400" onClick={addTask}> Add Task</button>
       </div>
-
-    
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
